c20fe331f1eef4edaa4d97bbd770062345da8032
pharzan <pharzan@outlook.com>
2016-07-28 17:58:14 +0300

    a

--- test.js
+++ test.js
@@ -4,3 +4,5 @@ var btn = document.createElement("BUTTON");
 var t = document.createTextNode("CLICK ME");       
 btn.appendChild(t);                                
 document.body.appendChild(btn);
+
+alert('hi');
