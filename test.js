8d0d7a2c2e3ea5f14a69be14484850cd75d97845
pharzan <pharzan@outlook.com>
2016-07-29 17:59:31 +0300

    a

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 console.log('pharzan-2 test')
-
+var a={{user.name}};
 var btn = document.createElement("BUTTON");        
 var t = document.createTextNode("CLICK ME");       
 btn.appendChild(t);                                
 document.body.appendChild(btn);
+console.log(a);
+
+
+
+
 
 
